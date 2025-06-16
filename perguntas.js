d11e03ada31eeff185b39cd297237908ce97cef2
ljaoxzz <joao.luiz.elias@escola.pr.gov.br>
2025-06-16 11:34:20 +0000

    ALTERACOES

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual o melhor time do mundo?',
+    'O melhor time do mundo é o Flamengo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital a Moldávia?',
+    'A capital da Moldávia é Chişinău'
+)
+
+criaCartao(
+    'jogo',
+    'Quando você da um HS em alguém o que você grita?',
+    'DUZENTOS!!!!!!!!!!!!!!!!!'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
